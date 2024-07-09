b9be8e6f06d4d30e8c8a4e2dfb4a8f5a5ebcf8f6
Nijeesh Joshy <nijeesh4all@gmail.com>
2024-07-09 23:05:55 +0530

    added job processing

--- spec/jobs/process_transactions_job_spec.rb
+++ spec/jobs/process_transactions_job_spec.rb
@@ -1,5 +1,31 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ProcessTransactionsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:transaction_ids) { [1, 2, 3] }
+  let(:users_with_pending_transactions) { [1, 2] }
+
+  describe '#perform' do
+    before do
+      allow(Transaction).to receive_message_chain(:state_pending, :where).and_return(Transaction.where(id: transaction_ids))
+      allow(Transaction).to receive_message_chain(:state_pending, :where, :distinct, :select, :pluck).and_return(users_with_pending_transactions)
+    end
+
+    it 'enqueues ProcessUserTransactionsJob for each user with pending transactions' do
+      users_with_pending_transactions.each do |user_id|
+        expect(ProcessUserTransactionsJob).to receive(:perform_later).with(user_id)
+      end
+
+      described_class.new.perform(transaction_ids)
+    end
+
+    it 'does not enqueue ProcessUserTransactionsJob if no users have pending transactions' do
+      allow(Transaction).to receive_message_chain(:state_pending, :where, :distinct, :select, :pluck).and_return([])
+
+      expect(ProcessUserTransactionsJob).not_to receive(:perform_later)
+
+      described_class.new.perform(transaction_ids)
+    end
+  end
 end
