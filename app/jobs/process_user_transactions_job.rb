955ac07ba65e47320f928b5eefcc7ebb93040a6d
Nijeesh Joshy <nijeesh.k@rently.com>
2024-07-09 23:03:43 +0530

    added job for processing user transactions

--- app/jobs/process_user_transactions_job.rb
+++ app/jobs/process_user_transactions_job.rb
@@ -1,6 +1,29 @@
 class ProcessUserTransactionsJob < ApplicationJob
   queue_as :default
 
+  sidekiq_options retry: 5,
+                  lock: :until_executed,
+                  on_conflict: {
+                                  client: :log,
+                                  server: :replace
+                                }
+
   def perform(user_id)
+    pending_transactions = Transaction.state_pending.where(user_id: user_id)
+    user = User.find(user_id)
+
+    pending_transactions.find_each do |transaction|
+      transaction.with_lock do
+        next unless transaction.state_pending?
+
+        user.with_lock do
+          user.points += transaction.points
+          user.save!
+        end
+
+        transaction.state_committed!
+      end
+    end
+
   end
 end
--- spec/jobs/process_user_transactions_job_spec.rb
+++ spec/jobs/process_user_transactions_job_spec.rb
@@ -1,5 +1,56 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ProcessUserTransactionsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user, points: 0) }
+  let!(:pending_transactions) do
+    create_list(:transaction, 2, user: user, state: 'pending', points: 50)
+  end
+  let!(:committed_transaction) do
+    create(:transaction, user: user, state: 'committed', points: 100)
+  end
+
+  describe '#perform' do
+    it 'processes pending transactions' do
+      expect {
+        described_class.new.perform(user.id)
+        user.reload
+      }.to change { user.points }.by(100)
+
+      pending_transactions.each do |transaction|
+        expect(transaction.reload.state).to eq('committed')
+      end
+    end
+
+    it 'does not process committed transactions' do
+      described_class.new.perform(user.id)
+
+      expect(committed_transaction.reload.state).to eq('committed')
+      expect(user.reload.points).not_to eq(150) # points remain unchanged from committed_transaction
+    end
+
+    it 'raises an error if user save fails' do
+      allow_any_instance_of(User).to receive(:save!).and_raise(ActiveRecord::RecordInvalid)
+
+      expect {
+        described_class.new.perform(user.id)
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  describe 'unique jobs' do
+    before do
+      Sidekiq::Testing.fake!
+    end
+
+    it 'processes only one job when duplicate jobs are enqueued' do
+      described_class.perform_later(user.id)
+      described_class.perform_later(user.id)
+
+      expect {
+        Sidekiq::Worker.drain_all
+      }.to change { user.reload.points }.by(100)
+    end
+  end
 end
