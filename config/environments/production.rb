baaaa073647df08be2b16449ee862c403b418718
Nijeesh Joshy <nijeesh4all@gmail.com>
2024-07-09 15:10:52 +0530

    added `config.hosts` to take the value from environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << ENV['HOST']
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
